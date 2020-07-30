93ea853a29ea9686df935a920820911dbbc13d34
Misha Memus <fedarovichmikhail@gmail.com>
2020-07-30 22:43:06 +0300

    init files

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const usersRouter = require('./routes/users.js');
+const cardsRouter = require('./routes/cards.js');
+
+
+const { PORT = 3000 } = process.env;
+const app = express();
+
+
+app.use('/', usersRouter);
+app.use('/', cardsRouter);
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`)
+})
+
+app.use(express.static(__dirname + '/public'));
+
---
+++ routes/users.js
@@ -0,0 +1,32 @@
+const usersRouter = require('express').Router();
+
+const users = require('../data/user.json');
+
+
+usersRouter.get('/users', (req, res) => {
+  res.send(users)
+});
+
+
+usersRouter.get('/users/:id', (req, res) => {
+  const { id } = req.params;
+
+  const arr = []
+
+  users.forEach(el => {
+    if (id === el._id) {
+      arr.push(el)
+      res.send(arr);
+    }
+  });
+
+  if (arr.length === 0) {
+    res.status(404);
+    res.send({ "message": "Нет пользователя с таким id" });
+  }
+
+
+
+});
+
+module.exports = usersRouter;
