b6a7a552d5bba3ab81d2e8f32bc9d0a20b7c3d0e
Misha Memus <fedarovichmikhail@gmail.com>
2020-08-06 02:11:27 +0300

    added fsPromises  module and edit find users

--- routes/cards.js
+++ routes/cards.js
@@ -1,9 +1,19 @@
 const cardsRouter = require('express').Router();
 
-const cards = require('../data/cards.json');
+const fsPromises = require('fs').promises;
+const path = require('path');
+
+const cardsPath = path.join(__dirname, '../data/cards.json');
 
 cardsRouter.get('/cards', (req, res) => {
-  res.send(cards);
+  fsPromises.readFile(cardsPath, { encoding: 'utf8' })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch(() => {
+      res.status(500);
+      res.send({ message: 'Внутренняя ошибка сервера' });
+    });
 });
 
 module.exports = cardsRouter;
--- routes/users.js
+++ routes/users.js
@@ -1,27 +1,34 @@
 const usersRouter = require('express').Router();
 
-const users = require('../data/user.json');
+const fsPromises = require('fs').promises;
+const path = require('path');
 
-usersRouter.get('/users', (req, res) => {
-  res.send(users);
-});
+const usersPath = path.join(__dirname, '../data/user.json');
 
 usersRouter.get('/users/:id', (req, res) => {
-  const { id } = req.params;
+  fsPromises.readFile(usersPath, { encoding: 'utf8' })
+    .then((data) => {
+      const users = JSON.parse(data);
+      const { id } = req.params;
+      // eslint-disable-next-line no-underscore-dangle
+      const user = users.filter((item) => item._id === id);
+      if (user.length === 0) {
+        res.status(404);
+        res.send({ message: 'Нет пользователя с таким id' });
+      }
+      res.send(user[0]);
+    });
+});
 
-  const arr = [];
-
-  users.forEach((el) => {
-    if (id === el._id) { // eslint-disable-line
-      arr.push(el);
-      res.send(arr);
-    }
-  });
-
-  if (arr.length === 0) {
-    res.status(404);
-    res.send({ message: 'Нет пользователя с таким id' });
-  }
+usersRouter.get('/users', (req, res) => {
+  fsPromises.readFile(usersPath, { encoding: 'utf8' })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch(() => {
+      res.status(500);
+      res.send({ message: 'Внутренняя ошибка сервера' });
+    });
 });
 
 module.exports = usersRouter;
